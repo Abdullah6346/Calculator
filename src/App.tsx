9741643fb459795de3ec3afdca7fb897e0ef8190
Muhammad Abdullah <139040726+Abdullah6346@users.noreply.github.com.>
2024-08-16 16:53:38 +0500

    feat: Added Handlers For Inputs

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,48 @@
 import "./App.css";
+import { useState } from "react";
 import Button from "./Components/Button";
 import Wrapper from "./Components/Wrapper";
 import ButtonBox from "./Components/ButtonBox";
 import Screen from "./Components/Screen";
+const btnnvalues = [
+  ["C", "+-", "%", "/"],
+  ["7", "8", "9", "X"],
+  ["4", "5", "6", "-"],
+  ["1", "2", "3", "+"],
+  ["0", ".", "="],
+];
 function App() {
-  const btnnvalues: array = [
-    ["C", "+-", "%", "/"],
-    ["7", "8", "9", "X"],
-    ["4", "5", "6", "-"],
-    ["1", "2", "3", "+"],
-    ["0", ".", "="],
-  ];
+  const [Cal, setCal] = useState({
+    sign: "",
+    res: 0,
+    num: 0,
+  });
+  const handleOnClick = () => {
+    btn === "C"
+      ? clearClickHandler
+      : btn === "+-"
+        ? invertClickHandler
+        : btn === "%"
+          ? percentClickHandler
+          : btn === "+" || "-" || "X" || "/"
+            ? signClickHandler
+            : btn === "."
+              ? commaClickHandler
+              : btn === "="
+                ? equalClickHandler
+                : numClickHandler;
+  };
   return (
     <>
       <Wrapper>
-        <Screen value={0} />
+        <Screen value={Cal.num ? Cal.num : Cal.res} />
         <ButtonBox>
           {btnnvalues.flat().map((btn, i: number) => {
             return (
               <Button
                 key={i}
                 className={btn === "=" ? "equals" : ""}
-                onClick={console.log(`${btn} ia clicked`)}
+                onClick={handleOnClick}
                 value={btn}
               />
             );
