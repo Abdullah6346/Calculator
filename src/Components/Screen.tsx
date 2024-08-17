fbdae26426e7949254de71ee214aaeb6090d4027
Muhammad Abdullah <139040726+Abdullah6346@users.noreply.github.com.>
2024-08-17 22:00:12 +0500

    fix: Fixed The Value Type For Screen

--- src/Components/Screen.tsx
+++ src/Components/Screen.tsx
@@ -1,10 +1,13 @@
 import { Textfit } from "react-textfit";
 
-const Screen = ({ value }) => {
+interface Screen {
+  value: number;
+}
+const Screen: React.FC<Screen> = ({ value }) => {
   return (
     <Textfit
       mode="single"
-      className="screen mb-2 box-border flex h-24 w-full content-center items-center justify-end bg-[#4357692d] px-10 py-0 font-bold text-white"
+      className="screen text-white mb-2 box-border flex h-24 w-full content-center items-center justify-end bg-[#4357692d] px-10 py-0 font-bold"
       id="display"
     >
       {value}
