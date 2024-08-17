7d5cba49112ee3c2ea4a23480bff29df4444a715
Muhammad Abdullah <139040726+Abdullah6346@users.noreply.github.com.>
2024-08-17 22:02:13 +0500

    fix: Fixed Wrapper Children

--- src/Components/Wrapper.tsx
+++ src/Components/Wrapper.tsx
@@ -1,4 +1,9 @@
-const Wrapper = ({ children }) => {
+import { ReactNode } from "react";
+
+interface Wrapper {
+  children: ReactNode;
+}
+const Wrapper: React.FC<Wrapper> = ({ children }) => {
   return (
     <div id="calculator" className="h-96 w-80 rounded-lg bg-[#485461]">
       {children}
